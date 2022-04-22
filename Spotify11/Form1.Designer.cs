6be23d880c91b49b7bc975ee0b5c3d4838fde494
nouran ahmed <64109399+nouran-ahmed-ebrahim@users.noreply.github.com>
2022-04-22 23:45:26 +0200

    initial commit

---
+++ Spotify11/Form1.Designer.cs
@@ -0,0 +1,154 @@
+﻿namespace Spotify11
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.searchTextBox = new System.Windows.Forms.TextBox();
+            this.searchBtn = new System.Windows.Forms.Button();
+            this.SongsList = new System.Windows.Forms.ListView();
+            this.SongName = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.song_Category = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.artistList = new System.Windows.Forms.ListView();
+            this.addToFavBtn = new System.Windows.Forms.Button();
+            this.AddToArtistBtn = new System.Windows.Forms.Button();
+            this.ArtistNameCln = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.SuspendLayout();
+            // 
+            // searchTextBox
+            // 
+            this.searchTextBox.Location = new System.Drawing.Point(70, 54);
+            this.searchTextBox.Name = "searchTextBox";
+            this.searchTextBox.Size = new System.Drawing.Size(380, 24);
+            this.searchTextBox.TabIndex = 0;
+            // 
+            // searchBtn
+            // 
+            this.searchBtn.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.searchBtn.Image = global::Spotify11.Properties.Resources.searchIcon;
+            this.searchBtn.Location = new System.Drawing.Point(546, 45);
+            this.searchBtn.Name = "searchBtn";
+            this.searchBtn.Size = new System.Drawing.Size(54, 49);
+            this.searchBtn.TabIndex = 1;
+            this.searchBtn.UseVisualStyleBackColor = true;
+            this.searchBtn.Click += new System.EventHandler(this.searchBtn_Click);
+            // 
+            // SongsList
+            // 
+            this.SongsList.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.SongName,
+            this.song_Category});
+            this.SongsList.HideSelection = false;
+            this.SongsList.Location = new System.Drawing.Point(70, 143);
+            this.SongsList.Name = "SongsList";
+            this.SongsList.Size = new System.Drawing.Size(530, 299);
+            this.SongsList.TabIndex = 2;
+            this.SongsList.UseCompatibleStateImageBehavior = false;
+            this.SongsList.View = System.Windows.Forms.View.Details;
+            // 
+            // SongName
+            // 
+            this.SongName.Text = "SongName";
+            this.SongName.Width = 100;
+            // 
+            // song_Category
+            // 
+            this.song_Category.Text = "song_Category";
+            this.song_Category.Width = 160;
+            // 
+            // artistList
+            // 
+            this.artistList.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.ArtistNameCln});
+            this.artistList.HideSelection = false;
+            this.artistList.Location = new System.Drawing.Point(70, 474);
+            this.artistList.Name = "artistList";
+            this.artistList.Size = new System.Drawing.Size(530, 150);
+            this.artistList.TabIndex = 3;
+            this.artistList.UseCompatibleStateImageBehavior = false;
+            this.artistList.View = System.Windows.Forms.View.Details;
+            // 
+            // addToFavBtn
+            // 
+            this.addToFavBtn.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.addToFavBtn.Font = new System.Drawing.Font("Times New Roman", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.addToFavBtn.Location = new System.Drawing.Point(788, 248);
+            this.addToFavBtn.Name = "addToFavBtn";
+            this.addToFavBtn.Size = new System.Drawing.Size(194, 60);
+            this.addToFavBtn.TabIndex = 5;
+            this.addToFavBtn.Text = "Add To Favorite List";
+            this.addToFavBtn.UseVisualStyleBackColor = true;
+            // 
+            // AddToArtistBtn
+            // 
+            this.AddToArtistBtn.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.AddToArtistBtn.Font = new System.Drawing.Font("Times New Roman", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.AddToArtistBtn.Location = new System.Drawing.Point(788, 513);
+            this.AddToArtistBtn.Name = "AddToArtistBtn";
+            this.AddToArtistBtn.Size = new System.Drawing.Size(194, 59);
+            this.AddToArtistBtn.TabIndex = 6;
+            this.AddToArtistBtn.Text = "Add To Artist List";
+            this.AddToArtistBtn.UseVisualStyleBackColor = true;
+            // 
+            // ArtistNameCln
+            // 
+            this.ArtistNameCln.Text = "Artist_Name";
+            this.ArtistNameCln.Width = 250;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1079, 784);
+            this.Controls.Add(this.AddToArtistBtn);
+            this.Controls.Add(this.addToFavBtn);
+            this.Controls.Add(this.artistList);
+            this.Controls.Add(this.SongsList);
+            this.Controls.Add(this.searchBtn);
+            this.Controls.Add(this.searchTextBox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox searchTextBox;
+        private System.Windows.Forms.Button searchBtn;
+        private System.Windows.Forms.ListView SongsList;
+        private System.Windows.Forms.ListView artistList;
+        private System.Windows.Forms.Button addToFavBtn;
+        private System.Windows.Forms.Button AddToArtistBtn;
+        private System.Windows.Forms.ColumnHeader SongName;
+        private System.Windows.Forms.ColumnHeader song_Category;
+        private System.Windows.Forms.ColumnHeader ArtistNameCln;
+    }
+}
+
---
+++ Spotify11/Form1.cs
@@ -0,0 +1,232 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Oracle.DataAccess.Client;
+using Oracle.DataAccess.Types;
+
+namespace Spotify11
+{
+    public partial class Form1 : Form
+    {
+        string orclData = "Data source = orcl; User Id = scott; Password=tiger;";
+        OracleConnection connection;
+        OracleDataReader reader;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            connection = new OracleConnection(orclData);
+            connection.Open();
+        }
+
+    private void searchBtn_Click(object sender, EventArgs e)
+        {
+             reader = IsItSong();
+            if (reader.HasRows)
+            {
+                displaySongs();
+            }
+            else
+            {
+                reader = IsItArtist();
+                if (reader.HasRows)
+                {
+
+                }
+                else
+                {
+                    reader = IsItCategory();
+                    if (reader.HasRows)
+                    {
+                        findCategorysSongs();
+                        AddToArtistBtn.Visible = false;
+                        artistList.Visible = false; 
+                    }
+                    else
+                    {
+                        MessageBox.Show("No Data found!");
+                    }
+                }
+            }
+        
+        }
+       
+        private void findCategorysSongs()
+        {
+            OracleDataReader songsReader;
+            int categoryId = 0;
+            string categoryName = "";
+            while (reader.Read())
+            {
+                categoryId = Convert.ToInt32(reader[0].ToString());
+                categoryName = reader[1].ToString();
+            }
+            songsReader = getCategorySongs(categoryId);
+            displayAllSongsInCategory(songsReader , categoryName);
+        }
+
+        private void displayAllSongsInCategory(OracleDataReader songReader, string categoryName)
+        {
+            ListViewItem song;
+            string songName;
+
+            while (songReader.Read())
+            {
+                // Get song info
+                songName = (songReader[1].ToString());
+
+                //fill songs lIst view
+                song = new ListViewItem(songName);
+                song.SubItems.Add(categoryName);
+                SongsList.Items.Add(song);
+            }
+
+        }
+
+        private OracleDataReader getCategorySongs(int categoryId)
+        {
+            OracleDataReader reader = null;
+            OracleCommand command = new OracleCommand()
+            {
+                Connection = connection,
+                CommandText = "select * from songs where CATEGORYID = :Id"
+            };
+            command.Parameters.Add("id", categoryId.ToString());
+            reader = command.ExecuteReader();   
+
+            return reader;
+        }
+        private OracleDataReader IsItSong()
+        {
+            string command = "select * from songs where Lower(songName) = :searchTextBox";
+            OracleCommand command1 = new OracleCommand
+            {
+                Connection = connection,
+                CommandText = command
+            };
+            command1.Parameters.Add("songName", searchTextBox.Text.ToString().ToLower());
+            OracleDataReader dr = command1.ExecuteReader();
+
+            return dr;
+        }
+        private OracleDataReader IsItArtist()
+        {
+            string command = "select * from Artist where LOWER(name) = :searchTextBox";
+            OracleCommand command1 = new OracleCommand
+            {
+                Connection = connection,
+                CommandText = command
+            };
+            command1.Parameters.Add("songName", searchTextBox.Text.ToString().ToLower());
+
+            OracleDataReader dr = command1.ExecuteReader();
+
+            return dr;
+        }
+        private OracleDataReader IsItCategory()
+        {
+            string command = "select * from SONGSCATEGORY where LOWER(CATEGORYNAME) = :searchTextBox";
+            OracleCommand command1 = new OracleCommand
+            {
+                Connection = connection,
+                CommandText = command
+            };
+            command1.Parameters.Add("songName", searchTextBox.Text.ToString().ToLower());
+
+            OracleDataReader dr = command1.ExecuteReader();
+
+            return dr;
+        }
+
+        private void displaySongs()
+        {
+            var artists = new HashSet<string>();
+            ListViewItem song;
+            string  songName , categoryName, artistName;
+            int caregoryId , songId;
+            
+            while(reader.Read())
+            {
+                // Get song info
+                songId = Convert.ToInt32(reader[0].ToString());
+                songName = (reader[1].ToString()); 
+                caregoryId = Convert.ToInt32(reader[2].ToString());
+                categoryName = getCategoryName(caregoryId);
+                artistName = getSongArtist(songId);
+
+                //fill songs lIst view
+                song = new ListViewItem(songName);
+                song.SubItems.Add(categoryName);    
+                SongsList.Items.Add(song);
+
+                // collect songs artists;
+                artists.Add(artistName);    
+            }
+
+            displayArtist(artists);
+        }
+
+        private void displayArtist(HashSet<string> artists)
+        {
+
+            foreach(string artistName in artists)
+            {
+               artistList.Items.Add(artistName);
+            }
+        }
+        private string getCategoryName(int caregoryId)
+        {
+            string categoryName = null;
+            OracleCommand command = new OracleCommand
+            {
+                Connection = connection,
+                CommandText= "select categoryname from songscategory where  categoryid = :category_Id"
+                //    CommandType = CommandType.StoredProcedure,
+                //    CommandText = "GETCATEGORYNAME"
+            };
+
+            command.Parameters.Add("Id", caregoryId);
+            //  command.Parameters.Add("name", OracleDbType.Varchar2, ParameterDirection.Output);
+            //  command.ExecuteNonQuery();
+            //  categoryName = command.Parameters["name"].Value.ToString();
+            
+            OracleDataReader dr = command.ExecuteReader();
+            while (dr.Read())
+                categoryName = (string)dr[0].ToString();
+
+            return categoryName;
+        }
+
+        private string getSongArtist(int songId)
+        {
+            string artistName = null;
+            OracleCommand command = new OracleCommand
+            {
+                Connection = connection,
+               // CommandText = "select categoryname from songscategory where  categoryid = :category_Id"
+                  CommandType = CommandType.StoredProcedure,
+                  CommandText = "GETARTISTNAME"
+            };
+
+            command.Parameters.Add("Id", songId);
+            command.Parameters.Add("name", OracleDbType.Varchar2,60, ParameterDirection.Output);
+            command.ExecuteNonQuery();
+            artistName = command.Parameters["name"].Value.ToString();
+
+          //  OracleDataReader dr = command.ExecuteReader();
+          //  while (dr.Read())
+          //  categoryName = (string)dr[0].ToString();
+
+            return artistName;
+        }
+    }
+}
